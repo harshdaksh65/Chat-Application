fbbfaac20c81a7c80c60ba76084e09b1f4c1de23
Harsh Daksh <harshdaksh082@gmail.com>
2025-08-07 20:24:35 +0530

    axios fixed

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: import.meta.env.Backend_URL ||
+    baseURL: import.meta.env.VITE_BACKEND_URL ||
   (import.meta.env.MODE === 'development'
     ? 'http://localhost:3000/api/v1'
     : 'https://talkie-qm4i.onrender.com/api/v1'),
